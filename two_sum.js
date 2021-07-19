80c2e723d3328afc3b7a798013cdb6667380ac19
Zachary Romsdahl <zdromsdahl@gmail.com>
2021-07-19 14:18:02 -0700

    Complete twoSum

---
+++ two_sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let map = new Map(); //key: num, val: index
+
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (map.has(diff)) {
+      return [map.get(diff), i];
+    }
+    map.set(nums[i], i);
+  }
+};
